79e4f373d6adb9b6cc360b9ebb28d93ce21c4a63
Ayhan Hazar <cesitliilanlar@gmail.com>
2023-04-22 12:51:11 +0300

    Update twentytwentythree_functions.php

--- twentytwentythree_functions.php
+++ twentytwentythree_functions.php
@@ -1,10 +1,13 @@
 <?php 
 function addFooter(){
 ?>
+    
 <style>
 .rightControl {right:0px  !important; left:auto !important;}
 .lastNavLiElement {left: -100% !important;}
 </style>
+    
+    
 <script>
 function subMenuPositionControl() {
     var cS = 200; // body.width - 200
@@ -12,22 +15,23 @@ function subMenuPositionControl() {
     var bWidth = document.body.clientWidth; // Body width
     var classController = 'rightControl'; // [(Add or Remove) Style Class]
     var classIsLastItem = 'lastNavLiElement'; // for Last nav item [(Add or Remove) Style Class]
-    var classSubMenusLi = '.wp-block-navigation-submenu'; class name of if navigation li has submenu 
+    var classSubMenusLi = '.wp-block-navigation-submenu'; //class name of if navigation li has submenu 
     var classSubMenuContentUl = '.wp-block-navigation__submenu-container'; // submenu container class name
     document.querySelectorAll(classSubMenusLi).forEach(function(menu, mi) { // mi = index
         if (mi % 2 == 0) { // % without submenu selection 
-            console.log(mi); // % log menu index
+            console.log(mi); // % log menu index number
             if (bWidth - menu.getBoundingClientRect()['right'] <= cS) { 
-                console.log('+');  
+                console.log('+');  //  Right positioning of submenu  
                 menu.querySelectorAll(classSubMenuContentUl)[0].classList.add(classController);
             } else {
-                console.log('-'); 
+                console.log('-'); //  Left positioning of submenu  
                 menu.querySelectorAll(classSubMenuContentUl)[0].classList.remove(classController);
             }
         }
     });
-    
-    // Last nav li item submenu positon center 
+                                                                    
+                                                                    
+    // Last nav li item submenu positon center <<<<<<<İF LAST ITEM!!>>>>>>
     
     var navLastLiItem = document.querySelectorAll(classSubMenusLi)[document.querySelectorAll(classSubMenusLi).length - 2];
     if (navLastLiItem.getBoundingClientRect()['left'] <= lCS) {
